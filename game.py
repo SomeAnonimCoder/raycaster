1a38a510876f7ddfbbfe4e55d9b774c7e27f9082
GlubshevArtem <glubshev2001@mail.ru>
2019-02-16 12:04:50 +0300

    removed problem code

--- game.py
+++ game.py
@@ -29,23 +29,9 @@ class GameState:
         cellW = fb.w / map.w
         cellH = fb.h / map.h
 
-        i = 0
-        while i < fb.w / 2:
-            angle = player.view - player.fov / 2 + player.fov * i / (fb.w / 2);
-            j=0
-            while j<100:
-                x = player.x + j*(cos(angle))
-                y = player.y + j*(sin(angle))
-                if(int(x * cellW)<fb.w-1 and int(y * cellH)<fb.h-1):
-                    if(not self.map.empty(int(x),int(y))):break
-                    fb.setPixel(int(x * cellW), int(y * cellH), Color(190, 190, 190))
-                j+=.01
-            i += 2
-        for i in range(0, map.w-1):
-            for j in range(0, map.h-1):
-                if not map.empty(i,j):
-                    fb.drawRectangle(int(i*cellW), int(j*cellH), int((i+1)*cellW), int((j+1)*cellH), Color(0,0,0))
-        return fb
+
+
+
 
 
 map = Map()
